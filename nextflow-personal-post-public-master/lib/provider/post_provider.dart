98b3f4d510db241b8140d89561647c13ddafa731
Piyawat Sakdadet <piyawatbol@gmail.com>
2021-12-07 22:41:50 +0700

    เพิ่ม ฟังชั่น get

--- nextflow-personal-post-public-master/lib/provider/post_provider.dart
+++ nextflow-personal-post-public-master/lib/provider/post_provider.dart
@@ -3,6 +3,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 
-class PostProvider with ChangeNotifier{
-    List<String> _post  = [];
+class PostProvider with ChangeNotifier {
+  List<String> _posts = [];
+
+// List <String>  get posts {
+//     return _posts;
+//   }
+
+List<String> get post => _posts ; // เหมือนกันกับด้านบน 
+addNewPost(String post){
+  _posts.add(post) ;
+  notifyListeners(); 
+}
+
 }
