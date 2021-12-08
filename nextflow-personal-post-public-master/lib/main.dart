13aabcacd64a08d5372bd1083876645544d3204f
Piyawat Sakdadet <piyawatbol@gmail.com>
2021-12-08 22:02:05 +0700

    เพิ่ม multiple

--- nextflow-personal-post-public-master/lib/main.dart
+++ nextflow-personal-post-public-master/lib/main.dart
@@ -2,6 +2,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:nextflow_personal_post/pages/new_post_page.dart';
+import 'package:nextflow_personal_post/provider/post_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,13 +13,20 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Nextflow Personal Post',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return MultiProvider(
+      providers: <SingleChildWidget>[
+         ChangeNotifierProvider(create: (BuildContext context) {
+           return PostProvider();
+         } ),
+      ],
+      child: MaterialApp(
+        title: 'Nextflow Personal Post',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: MyHomePage(title: 'Personal Post'),
       ),
-      home: MyHomePage(title: 'Personal Post'),
     );
   }
 }
@@ -34,53 +44,67 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          actions: <Widget>[
-            IconButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (BuildContext context) {
-                      return NewPostPage();
-                    }),
-                  );
-                },
-                icon: Icon(Icons.add)),
-          ],
-        ),
-        body: ListView.builder(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (BuildContext context) {
+                    return NewPostPage();
+                  }),
+                );
+              },
+              icon: Icon(Icons.add)),
+        ],
+      ),
+      body: Consumer<PostProvider>(
+          builder: (BuildContext context, PostProvider provider, Widget child) {
+        return ListView.builder(
+          itemCount: provider.posts.length,
           itemBuilder: (BuildContext context, int index) {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(10),
-                  child: Column(
-                    children: [
-                      Text(
-                        '10 min ago',
-                        style: TextStyle(color: Colors.grey[400]),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        'สวัสดีโลก',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey[350],
-                    ),
-                  ),
-                ),
-              ],
-            );
+            return Text(provider.posts[index]);
           },
-        ));
+        );
+      }),
+
+
+      // body: ListView.builder(
+      //   itemCount: 1,
+      //   itemBuilder: (BuildContext context, int index) {
+      //     return Column(
+      //       crossAxisAlignment: CrossAxisAlignment.start,
+      //       children: [
+      //         Container(
+      //           padding: EdgeInsets.all(10),
+      //           child: Column(
+      //             children: [
+      //               Text(
+      //                 '10 min ago',
+      //                 style: TextStyle(color: Colors.grey[400]),
+      //               ),
+      //               SizedBox(height: 10),
+      //               Text(
+      //                 'สวัสดีโลก',
+      //                 style: TextStyle(fontSize: 20),
+      //               ),
+      //             ],
+      //           ),
+      //         ),
+      //         SizedBox(
+      //           height: 10,
+      //           child: Container(
+      //             decoration: BoxDecoration(
+      //               color: Colors.grey[350],
+      //             ),
+      //           ),
+      //         ),
+      //       ],
+      //     );
+      //   },
+      // )
+      
+    );
   }
 }
